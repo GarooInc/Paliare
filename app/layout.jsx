cdda09e897827d0b087ac402752ca15d4b03e170
Mark Albrand <mark.albrand5@gmail.com>
2024-03-06 09:53:41 -0600

    Setup

---
+++ app/layout.jsx
@@ -0,0 +1,33 @@
+import "@/styles/globals.css"
+
+export const metadata = {
+    title: 'Paliare',
+    description: 'Especializada en el campo del marketing digital, centrada en la estrategia de optimización de la presencia de nuestros clientes en Google Maps.',
+    url: '',
+    image: '/assets/images/logo.png',
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang="es">
+        <head>
+            <title>{metadata.title}</title>
+            <link rel="icon" type="image/png+xml" href="/assets/images/favicon.png" />
+            <meta name="description" content={metadata.description} />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta property="og:title" content={metadata.title} />
+            <meta property="og:description" content={metadata.description} />
+            <meta property="og:image" content={metadata.image} />
+            <meta property="og:url" content={metadata.url} />
+            <meta name="keywords" content="proyectos, propiedad inmobiliaria, Guatemala, "></meta>            
+        </head>
+        <body>
+            <main className='app'>
+                  {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {   
+    return (
+        <main className="flex min-h-screen w-full flex-col items-center justify-between bg-black">
+            <h1 className="text-blue-500">Paliare</h1>
+        </main>
+    )
+}
