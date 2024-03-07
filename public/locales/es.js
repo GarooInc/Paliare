372f30c019f385dd6b0c2a8318c3be22b856983f
Mark Albrand <mark.albrand5@gmail.com>
2024-03-07 09:30:16 -0600

    Homepage, filosofía, servicios, diseño de arquitectura

--- public/locales/es.js
+++ public/locales/es.js
@@ -1,5 +1,58 @@
 const textsES = {
     title: "¡Hola, Mundo!",
+    // Homepage
+    homepage_title: "Creamos lugares para vivir y sentir:",
+    homepage_subtitle: "Diseño de arquitectura, interiorismo y construcción",
+    homepage_kwowmore: "Conoce Paliare",
+
+    // Filosofía
+    philosophy_header: "Filosofía",
+    philosophy_title: "Todos somos hormigas",
+    philosophy_text: "El mejor trabajo es el que se hace en equipo y hoy estamos aquí gracias a todas las personas que han dejado su grano de arena en Paliare",
+
+    // Servicios
+    services_header: "Servicios",
+    services_title: "Este es el principio de la creatividad",
+    services_subtitle: "Seguimos un proceso que forma vínculos entre las personas y los lugares que habitan",
+    services_text: "Como cualquier transformación, para llegar al destino final recorremos un camino no lineal",
+    services_title_2: "Si algo podemos prometer es la confianza",
+    services_subtitle_2: "Esta es una gran promesa",
+    services_text_2: "En la vida, los proceos llevan el tiempo requerido para llegar a su destino funal y lo mismo sucede con la arquitectura. Aquí te acompañamos en cada etapa de inicio a fin de la mano de la sendibilidad de expertos.",
+    
+    services_service_proyects: "Ver proyectos",
+    services_service_1_title: "Diseño de arquitectura",
+    services_service_2_title: "Interiorismo",
+    services_service_3_title: "Construcción",
+    
+    services_section_2_title: "Estás en el lugar indicado cuando:",
+    services_section_2_text_1: "Buscas espacios",
+    services_section_2_text_1_1: "Que te hacen sentir bienvenido",
+    services_section_2_text_2: "Sueñas con expresar",
+    services_section_2_text_2_1: "Tus ideas en la vida real",
+    services_section_2_text_3: "Quieres sentirte guiado",
+    services_section_2_text_3_1: "Y acompañado en cada etapa del proceso",
+    services_section_2_text_4: "Anhelas confiar en",
+    services_section_2_text_4_1: "El diseño y la ejecución",
+
+    services_process_header: "Proceso Paliare",
+    services_process_title_1: "Nos conocemos",
+    services_process_text_1: "Tenemos una conversación incial para que nos cuentes acerca de tus ideas y tu estilo de vida. Con estos detalles empezamos a explorar diseños potenciales para tí.",
+    services_process_title_2: "Diseñamos tu espacio",
+    services_process_text_2: "Trabajamos juntos en cada parte del proceso para co-diseñar una obra que refleje tu personalidad.",
+    services_process_title_3: "De los planos a la vida real",
+    services_process_text_3: "Vive la experiencia de tu espacio en persona.",
+
+    // Diseño de arquitectura
+    design_header: "Proyectos",
+    design_title: "Diseño de arquitectura",
+    design_text1: "A la vanguardia de técnicas nuevas, materiales innovadores y ua mentalidad de sostenibilidad en el tiemp, diseñamos espacios atemporales que entrelazan el arte, la naturaleza y la arquitectura.",
+    design_quote: "Cotizar",
+
+    // Interiorismo
+    interiorism_header: "Proyectos",
+    interiorism_title: "Interiorismo",
+    interiorism_text1: "Entendemos a profundidad la dinámica entre"
+
 }
 
 export { textsES }
