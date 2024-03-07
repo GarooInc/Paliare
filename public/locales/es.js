c656f3117ff8e1fe8411f9eaabe722ef10e4fbd8
Mark Albrand <mark.albrand5@gmail.com>
2024-03-07 10:04:32 -0600

    Update es.js

--- public/locales/es.js
+++ public/locales/es.js
@@ -1,14 +1,77 @@
 const textsES = {
     title: "¡Hola, Mundo!",
+
+    // Menu
+    menu_home: "Inicio",
+    menu_philosophy: "Filosofía",
+    menu_projects: "Proyectos",
+    menu_services: "Servicios",
+    menu_contact: "Contacto",
+
+    // Footer
+    footer_home: "Inicio",
+    footer_philosophy: "Filosofía",
+    footer_projects: "Proyectos",
+    footer_services: "Servicios",
+    footer_contact: "Contacto",
+    footer_text: "Creando espacios para vivir y sentir de forma auténtica.",
+
     // Homepage
     homepage_title: "Creamos lugares para vivir y sentir:",
     homepage_subtitle: "Diseño de arquitectura, interiorismo y construcción",
     homepage_kwowmore: "Conoce Paliare",
+    homepage_section_1_title_1: "El puente que une",
+    homepage_section_1_title_2: "las ideas y la vida real",
+    homepage_section_1_text_1: "Te llevamos por la experiencia de lo que significa crear espacios intuitivos.",
+    homepage_section_1_subtitle: "+20 años",
+    homepage_section_1_text_2: "Dándole vida a proyectos intencionales",
+
+    homepage_section_2_title: "Arquitectura",
+    homepage_section_2_subtitle: "Un proceso creativo, tres enfoques",
+    homepage_section_2_text_1: "Diseñamos espacios funcionales en donde el estilo de vida de cada cliente juega el rol principal a la hora de escoger los materiales, la iluminación y el juego volumétrico del proyecto.",
+    homepage_section_2_slide_1_title: "Arquitectura",
+    homepage_section_2_slide_1_text: "Diseñamos espacios funcionales en donde el estilo de vida de cada cliente juega el rol principal a la hora de escoger los materiales, la iluminación y el juego volumétrico del proyecto.",
+    homepage_section_2_slide_2_title: "Interiorismo",
+    homepage_section_2_slide_2_text: "Se trata de la práctica artística de encontrar el balance entre la decoración, gusto y funcionalidad en espacios interiores. Aquí se realiza la dinámica personal e íntima de cada cliente.",
+    homepage_section_2_slide_3_title: "Construcción",
+    homepage_section_2_slide_3_text: "El detalle de la construcción es nuestra pasión",
+    homepage_section_2_slides_more: "Leer más",
+
+    homepage_section_3_title_1: "Somos un equipo de profesionales y creativos con el mismo destino:",
+    homepage_section_3_title_2: "Lugares auténticos.",
+    homepage_section_3_title_3: "Experiencias únicas.",
+
+    homepage_section_4_title: "El corazón de Paliare",
+    homepage_section_4_subtitle: "Las grandes obras son un trabajo de hormigas y aquí cada hormiga juega un rol esencial.",
+    homepage_section_4_text_1: "Los arquitectos, los ingenieros y los diseñadores de Paliare entrelazan sus habilidades para convertir todas (si, todas) las ideas en realidad.",
+    homepage_section_4_text_2: "Reconocemos que se trata de un proceso en donde la",
+    homepage_section_4_text_2_enphasis: "CONFIANZA",
+    homepage_section_4_text_2_2: "es el elemento principal. Por suerte, la confianza de los clientes ha sido nuestras mejores fortalezas en los útlmos 20 años.",
+    homepage_section_4_kwnowmore: "Conócenos",
+
+    homepage_section_5_title: "5 premios iberoamericanos de arquitectura e interiorismo,",
+    homepage_section_5_subtitle: "proyectos exclusivos residenciales a nivel regional y más de 20 años haciendo lo que no gusta.",
+    homepage_section_5_text_1: "Con la creatividad al centro de sus proyectos, Patty comenzó Paliare para diseñar espacios en donde las personas pudieran desenvolverse en su versión más auténtica.",
+    homepage_section_5_text_2: "Patricia Arenas",
+    homepage_section_5_text_3: "¿Conversamos?",
+
 
     // Filosofía
     philosophy_header: "Filosofía",
     philosophy_title: "Todos somos hormigas",
-    philosophy_text: "El mejor trabajo es el que se hace en equipo y hoy estamos aquí gracias a todas las personas que han dejado su grano de arena en Paliare",
+    philosophy_text_1: "El mejor trabajo es el que se hace en equipo y hoy estamos aquí gracias a todas las personas que han dejado su grano de arena en Paliare",
+    philosophy_text_2: "Desde que Paliare empezó hace +20 años, sus proyectos se caracterizan por el sentimiento que provocan: armonía.",
+    philosophy_subtitle: "Haría esto aunque no fuera mi trabajo",
+    philosophy_text_3: "Patricia Arenas",
+
+    philosophy_section_2_text_1: "Siguiendo la filosofía de las hormigas",
+    philosophy_section_2_text_2: "Creemos que los proyectos son el resultado de una combinación del",
+    philosophy_section_2_text_2_1: "Sueño del cliente",
+    philosophy_section_2_text_2_2: "El talento del equipo",
+    philosophy_section_2_text_2_3: "La diligencia profesional",
+    philosophy_section_2_text_3: "En donde el corazón late despacio, es aquel lugar en donde existe un tipo de serenidad que solo se siente en el verdadero hogar.",
+    philosophy_section_2_text_4: "Descubre como vamos más allá del espacio físico",
+
 
     // Servicios
     services_header: "Servicios",
@@ -51,7 +114,34 @@ const textsES = {
     // Interiorismo
     interiorism_header: "Proyectos",
     interiorism_title: "Interiorismo",
-    interiorism_text1: "Entendemos a profundidad la dinámica entre"
+    interiorism_text1: "Entendemos a profundidad la dinámica entre ambientes para diseñar espacios que reflejen la energía, la estética y el corazón de cada proyecto.",
+    interiorism_quote: "Cotizar",
+
+    // Construcción
+    construction_header: "Proyectos",
+    construction_title: "Terraré",
+    construction_text1: "Encargados de asesorar y supervisar las obras de principio a fin. Terraré se encarga del brazo de construcción de cada proyecto.",
+    construction_quote: "Cotizar",
+
+    // Contacto
+    contact_header: "Contacto",
+    contact_title: "Conversemos",
+    contact_form_name: "Nombre",
+    contact_form_email: "Correo electrónico",
+    contact_form_phone: "Teléfono/Celular",
+    contact_form_service: "Servicio de interés",
+    contact_form_message: "Mensaje",
+    contact_form_send: "Enviar",
+    contact_form_required: "Campo requerido",
+    contact_subtitle_1: "Las grandes obras no son hechas con fuerza, sino con la perseverancia",
+    contact_subtitle_2: "Ese es el trabajo de hormigas.",
+
+    // Cotizador de servicios
+    quote_header: "Déjanos tus datos",
+    quote_form_name: "Nombre",
+    quote_form_email: "Correo electrónico",
+    quote_form_phone: "Teléfono/Celular",
+    quote_form_send: "Enviar",
 
 }
 
