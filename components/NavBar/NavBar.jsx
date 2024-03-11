9885c37cd74c1865fa8497cda8fa862b51486889
Jimena Hernández <her21199@uvg.edu.gt>
2024-03-11 12:36:44 -0600

    Update NavBar.jsx

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -45,10 +45,10 @@ const NavBar = ({primary}) => {
           <div className="flex ">
             <button
               type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-black"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-black focus:outline-none" 
               onClick={() => setMobileMenuOpen(true)}
             >
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              <Bars3Icon className="h-6 w-6 focus:outline-none" aria-hidden="true" />
             </button>
           </div>
         </nav>
@@ -64,7 +64,7 @@ const NavBar = ({primary}) => {
                 />
               <button
                 type="button"
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                className="-m-2.5 rounded-md p-2.5 text-gray-700 focus:outline-none"
                 onClick={() => setMobileMenuOpen(false)}
               >
                 <span className="sr-only">Close menu</span>
