7baf307371de2e5d9ec8e6fb0fb06630466d3d35
Jimena Hernández <her21199@uvg.edu.gt>
2024-03-13 09:55:50 -0600

    update proyects

--- app/[locale]/construccion/page.jsx
+++ app/[locale]/construccion/page.jsx
@@ -16,18 +16,14 @@ export default async function Construccion({ params: { locale }}) {
         <TranslationsProvider locale={locale} namespaces={namespaces} resources={resources}>
         <NavBar transparent />
         <div className='flex flex-col w-full bg-white'>
-            <ImageBanner image="/assets/images/construccion/portada.jpg" right navRight="/proyectos" left navLeft="/proyectos" text={t('construction_title')} />
+            <ImageBanner image="/assets/images/construccion/portada.jpg" right navRight="/proyectos" left navLeft="/proyectos" logo="/assets/images/construccion/Terrare.png" />
             <div className="flex w-full bg-white flex-col normal_section">
                 <span className="mont text-black text-center pt-10">{t('construction_text1')}</span>
-                <div className="flex flex-col gap-4 py-10">
-                    <img src="/assets/images/arquitectura/2.jpg" alt="arquitectura1" className="w-full lg:h-[500px] h-[300px] object-cover" />
-                    <div className="flex flex-row gap-4">
-                        <div className='flex flex-col gap-4 justify-between'>
-                            <img src="/assets/images/arquitectura/3.jpg" alt="arquitectura2" className=" lg:h-[300px] h-[150px] object-cover" />
-                            <img src="/assets/images/arquitectura/3.jpg" alt="arquitectura3" className="lg:h-[300px] h-[150px] object-cover" />
-                        </div>
-                        <img src="/assets/images/arquitectura/1.jpg" alt="arquitectura4" className="w-1/2 lg:h-[620px] h-[320px] object-cover" />
-                    </div>
+                <div className="grid grid-cols-2 grid-rows-2 gap-4 py-10">
+                    <img src="/assets/images/construccion/1.jpg" alt="construccion1" className="w-full lg:h-[400px] h-[300px] object-cover" />
+                    <img src="/assets/images/construccion/2.png" alt="construccion2" className="w-full lg:h-[400px] h-[300px] object-cover" />
+                    <img src="/assets/images/construccion/3.jpg" alt="construccion3" className="w-full lg:h-[300px] h-[200px] object-cover" />
+                    <img src="/assets/images/construccion/4.png" alt="construccion4" className="w-full lg:h-[300px] h-[200px] object-cover" />
                 </div>
             </div>
             <LanguageSwitcher />
--- components/ImageBanner/ImageBanner.jsx
+++ components/ImageBanner/ImageBanner.jsx
@@ -6,7 +6,7 @@ import { IoIosArrowRoundForward } from "react-icons/io"
 import { IoIosArrowRoundBack } from "react-icons/io"
 
 
-const ImageBanner = ({image, right, left, navRight, navLeft, text}) => {
+const ImageBanner = ({image, right, left, navRight, navLeft, text, logo}) => {
     const router = useRouter()
     const { t } = useTranslation()
   return (
@@ -24,7 +24,13 @@ const ImageBanner = ({image, right, left, navRight, navLeft, text}) => {
             onClick={() =>  router.push(navLeft)}
             />
         }
-        <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 tittle_v2 text-white font-bold">{text}</span>
+        {
+          text && <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 tittle_v2 text-white font-bold">{text}</span>
+
+        }
+        {
+          logo && <img src={logo} alt="logo" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-60 w-auto" />
+        }
     </div>
   )
 }
