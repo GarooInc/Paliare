bc52eea75b805ac8f036c499ad6eabfbcf407169
Jimena Hernández <her21199@uvg.edu.gt>
2024-03-18 07:37:18 -0600

    update

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -1,12 +1,11 @@
 import "@/styles/globals.css"
-import Footer from "@/components/Footer/Footer"
 
 
 export const metadata = {
     title: 'Paliare',
-    description: 'Especializada en el campo del marketing digital, centrada en la estrategia de optimización de la presencia de nuestros clientes en Google Maps.',
+    description: 'Somos estudio de arquitectura e interiorismo. Generamos experiencias únicas a través de nuestros espacios y diseños.',
     url: '',
-    image: '/assets/images/logo.png',
+    image: '/assets/images/paliareblanco.png',
 }
 
 const RootLayout = ({children}) => {
@@ -14,14 +13,14 @@ const RootLayout = ({children}) => {
     <html lang="es">
         <head>
             <title>{metadata.title}</title>
-            <link rel="icon" type="image/png+xml" href="/assets/images/favicon.png" />
+            <link rel="icon" type="image/png+xml" href="/assets/images/paliarenegro.png" />
             <meta name="description" content={metadata.description} />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta property="og:title" content={metadata.title} />
             <meta property="og:description" content={metadata.description} />
             <meta property="og:image" content={metadata.image} />
             <meta property="og:url" content={metadata.url} />
-            <meta name="keywords" content="proyectos, propiedad inmobiliaria, Guatemala, "></meta>            
+            <meta name="keywords" content="estudio, propiedad interiorismo, Guatemala, "></meta>            
         </head>
         <body>
             <main className='app'>
