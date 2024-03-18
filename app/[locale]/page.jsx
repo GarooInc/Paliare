2981485fccb544c2879f3f881ace2d86cc1e35fb
Jimena Hernández <her21199@uvg.edu.gt>
2024-03-18 10:22:52 -0600

    update

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -55,18 +55,18 @@ export default async function Home({ params: { locale }}) {
             </section>
             <section className="normal_section pt-20 min-h-screen bg-white">
                 <div className="w-full flex lg:justify-start justify-center">
-                    <h1 className="text-3xl mont lg:flex lg:flex-col lg:w-1/3 text-center lg:text-start">
-                        <span className='text-black font-bold mr-2'>{t('homepage_section_1_title_1')}</span>
-                        <span className='text-black font-light'>{t('homepage_section_1_title_1_1')}</span>
+                    <h1 className="lg:text-3xl text-xl mont lg:w-1/4 text-center lg:text-start">
+                        <span className='text-black font-bold mr-2 uppercase'>{t('homepage_section_1_title_1')}</span>
+                        <span className='text-black font-light uppercase'>{t('homepage_section_1_title_1_1')}</span>
                     </h1>
                 </div>
                 <div className='relative w-full flex flex-col py-10'>
                     <img src="/assets/images/homepage/puente.svg" alt="" className="w-full lg:h-[600px] " />
                     <span className="text-black mont lg:text-start text-center text-md lg:w-1/4 py-10 lg:absolute lg:right-0 lg:bottom-60">{t('homepage_section_1_text_1')}</span>
                 </div>
-                <div className="w-full flex_center gap-4 py-10">
-                    <span className="text-black text-5xl anton font-bold uppercase">{t('homepage_section_1_text_2')}</span>
-                    <span className="text-black mont text-center text-md lg:text-start lg:w-1/6 w-1/2">{t('homepage_section_1_text_3')}</span>
+                <div className="w-full flex_center gap-4 py-10 lg:pb-40">
+                    <span className="text-black text-5xl anton font-bold uppercase ">{t('homepage_section_1_text_2')}</span>
+                    <span className="text-black mont text-center text-md lg:text-start lg:w-1/6 w-1/2 ">{t('homepage_section_1_text_3')}</span>
                 </div>
             </section>
             <div className="bg-darkgray w-full p-4 flex_center">
--- components/Carousel/Carousel.jsx
+++ components/Carousel/Carousel.jsx
@@ -19,7 +19,7 @@ const Carousel = ({images}) => {
                       {
                         images[index+1] ? <a href={`#${index+1}`} className="absolute top-1/2 right-10 transform -translate-y-1/2"> <IoIosArrowRoundForward className='text-white text-6xl'/> </a> : <a href={`#0`} className=" absolute top-1/2 right-10 transform -translate-y-1/2"> <IoIosArrowRoundForward className='text-white text-6xl'/> </a>
                       }
-                      <h1 className="absolute top-1/2 left-20 text-white text-6xl font-regular mont uppercase m-0">{image.title}</h1>
+                      <h1 className="absolute top-1/2 left-20 text-white text-6xl font-regular mont uppercase m-0 tracking-widest">{image.title}</h1>
                       <div className="absolute bottom-20 right-20 text-white text-2xl font-light mont w-1/3 flex flex-col">
                         <span className="text-white text-lg  mont font-medium">{image.desc}</span>
                         <button className="text-start text-white text-lg font-medium underline mont" onClick={() => router.push(image.path)}> {t('homepage_section_2_slides_more') }</button>
