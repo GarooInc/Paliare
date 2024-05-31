cab85c5b6489d8d612119559afd1f8d30f5e7307
Jimena Hernández <her21199@uvg.edu.gt>
2024-05-31 11:42:54 -0600

    Update Carousel.jsx

--- components/Carousel/Carousel.jsx
+++ components/Carousel/Carousel.jsx
@@ -15,7 +15,7 @@ const Carousel = ({images, text}) => {
                 images.map((image, index) => (
                     <div key={index} id={index} className="carousel-item flex justify-center w-full relative">
                         <img src={image.src} className="w-full lg:h-[800px] h-[400px] object-cover brightness-50" alt={image.alt} />
-                        <div className='flex flex-col absolute lg:top-1/2 top-40 gap-6 justify-center items-center'>
+                        <div className='flex flex-col absolute lg:top-1/2 top-40 gap-6 justify-center items-center lg:px-0 px-8'>
                             <h1 className="text-white md:text-6xl text-4xl font-regular m-0 anton uppercase text-center tracking-widest">{image.title}</h1>
                             <a href={image.path} className="text-white md:text-lg text-sm font-bold mont flex flex-col underline tracking-wider">
                                 {text}
